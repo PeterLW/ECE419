ba3d218e165062ca85a9e96c861ef6c47f5ccde2
Cathy Y Fu <cathyyfu@gmail.com>
2018-02-24 18:14:22 -0500

    dependencies

--- main_code/src/ecs/ServerManager.java
+++ main_code/src/ecs/ServerManager.java
@@ -1,58 +1,62 @@
 package ecs;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.TreeMap;
+
 
 public class ServerManager {
-    // hash values (start) -> serverNode
-    private TreeMap<String,ServerNode> tree = new TreeMap<String,ServerNode>();
-    // ip:port -> serverNode
-    private HashMap<String,ServerNode> hashMap = new HashMap<String,ServerNode>();
+//    // hash values (start) -> serverNode
+//    private TreeMap<String,ServerNode> tree = new TreeMap<String,ServerNode>();
+//    // ip:port -> serverNode
+//    private HashMap<String,ServerNode> hashMap = new HashMap<String,ServerNode>();
 
     public ServerManager(){
 
     }
 
-    public boolean addNode(ServerNode n){
-        String id = n.getNodeId(); // ip:port
-        if (hashMap.containsKey(id)) {
-            return false;
-        }
-        hashMap.put(id,n);
 
-        return true;
-    }
 
-    /**
-     * @return number of nodes successfully added
-     */
-    public int addNodes(List<ServerNode> nodes){
-        return 0;
-    }
 
-    public boolean removeNode(String id) {
-        ServerNode n = hashMap.get(id);
-        if (n == null){
-            return false;
-        }
-        String hash = n.getNodeHashRange()[0];
 
-        if(!tree.containsKey(hash)){
-            hashMap.remove(id);
-            throw new RuntimeException(id + " was found in hashMap but not in tree");
-        }
 
-        hashMap.remove(id);
-        tree.remove(hash);
-        return true;
-    }
-
-    /**
-     * @return number of nodes successfully removed
-     */
-    public int removeNodes(List<String> ids){
-        return 0;
-    }
+//
+//    public boolean addNode(ServerNode n){
+//        String id = n.getNodeId(); // ip:port
+//        if (hashMap.containsKey(id)) {
+//            return false;
+//        }
+//        hashMap.put(id,n);
+//
+//        return true;
+//    }
+//
+//    /**
+//     * @return number of nodes successfully added
+//     */
+//    public int addNodes(List<ServerNode> nodes){
+//        return 0;
+//    }
+//
+//    public boolean removeNode(String id) {
+//        ServerNode n = hashMap.get(id);
+//        if (n == null){
+//            return false;
+//        }
+//        String hash = n.getNodeHashRange()[0];
+//
+//        if(!tree.containsKey(hash)){
+//            hashMap.remove(id);
+//            throw new RuntimeException(id + " was found in hashMap but not in tree");
+//        }
+//
+//        hashMap.remove(id);
+//        tree.remove(hash);
+//        return true;
+//    }
+//
+//    /**
+//     * @return number of nodes successfully removed
+//     */
+//    public int removeNodes(List<String> ids){
+//        return 0;
+//    }
 
 }
