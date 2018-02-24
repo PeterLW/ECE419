5fc7f3bc1c74a7aeb9173c27417342449c7b4579
Cathy Y Fu <cathyyfu@gmail.com>
2018-02-23 23:22:03 -0500

    server node

---
+++ main_code/src/common/ecs/ServerManager.java
@@ -0,0 +1,20 @@
+package common.ecs;
+
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class ServerManager {
+    private TreeSet<ServerNode> tree = new TreeSet<ServerNode>(new Comparator<ServerNode>() {
+
+        @Override
+        public int compare(ServerNode o1, ServerNode o2) {
+            if ()
+        }
+
+    });
+
+    public ServerManager(){
+
+    }
+}
--- main_code/src/common/ecs/ServerNode.java
+++ main_code/src/common/ecs/ServerNode.java
@@ -1,19 +1,35 @@
 package common.ecs;
 
 public class ServerNode implements IECSNode {
+    private String name;
+    private String host;
+    private String id;
+    private int port;
+
+    public ServerNode(String name, String host, int port){
+        this.name = name;
+        this.host = host;
+        this.port = port;
+        this.id = host + ":" + Integer.toString(port);
+    }
+
     @Override
     public String getNodeName() {
-        return null;
+        return name;
     }
 
     @Override
     public String getNodeHost() {
-        return null;
+        return host;
+    }
+
+    public String getNodeHostPort(){
+        return id;
     }
 
     @Override
     public int getNodePort() {
-        return 0;
+        return port;
     }
 
     @Override
