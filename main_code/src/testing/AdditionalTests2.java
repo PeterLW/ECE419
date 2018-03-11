139ece96f0efd99f1cbdfc55a670ba51c121697b
Cathy Y Fu <cathyyfu@gmail.com>
2018-03-11 17:37:42 -0400

    fix test

--- main_code/src/testing/AdditionalTests2.java
+++ main_code/src/testing/AdditionalTests2.java
@@ -10,8 +10,6 @@ import junit.framework.TestCase;
 public class AdditionalTests2 extends TestCase {
 
     private void fct(ServerNode n){
-        System.out.println(n.getServerStatus().getTransition().name());
-
         ServerStatus s = new ServerStatus(ZNodeMessageStatus.NEW_ZNODE_RECEIVE_DATA);
         s.setServerStatus(ServerStatusType.MOVE_DATA_SENDER);
         n.setServerStatus(s);
@@ -25,8 +23,8 @@ public class AdditionalTests2 extends TestCase {
         ServerStatus s = new ServerStatus(ZNodeMessageStatus.NEW_ZNODE);
         n.setServerStatus(s);
         fct(n);
-        assertEquals(ZNodeMessageStatus.NEW_ZNODE_RECEIVE_DATA,n.getServerStatus().getTransition().name());
-        assertEquals(ServerStatusType.MOVE_DATA_SENDER,n.getServerStatus().getStatus().name());
+        assertEquals(0,n.getServerStatus().getTransition().compareTo(ZNodeMessageStatus.NEW_ZNODE_RECEIVE_DATA));
+        assertEquals(0,n.getServerStatus().getStatus().compareTo(ServerStatusType.MOVE_DATA_SENDER));
     }
 
 }
