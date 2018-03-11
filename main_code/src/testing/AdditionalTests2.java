940a25322132eb38a6aa3ada8b29c3a02754e59d
Cathy Y Fu <cathyyfu@gmail.com>
2018-03-11 05:26:32 -0400

    tests

--- main_code/src/testing/AdditionalTests2.java
+++ main_code/src/testing/AdditionalTests2.java
@@ -2,13 +2,12 @@ package testing;
 
 import app_kvServer.ServerStatus;
 import app_kvServer.ServerStatusType;
-import common.zookeeper.ZNodeMessage;
 import common.zookeeper.ZNodeMessageStatus;
 import ecs.ConfigEntity;
 import ecs.ServerNode;
 import junit.framework.TestCase;
 
-public class RandomTest extends TestCase {
+public class AdditionalTests2 extends TestCase {
 
     private void fct(ServerNode n){
         System.out.println(n.getServerStatus().getTransition().name());
@@ -21,15 +20,15 @@ public class RandomTest extends TestCase {
     }
 
     //@Test
-    public void testStat(){
+    public void testSettingStatus(){
         ConfigEntity e = new ConfigEntity("Server1","localhost", 50000);
         ServerNode n = new ServerNode(e,100,"LFU");
 
         ServerStatus s = new ServerStatus(ZNodeMessageStatus.NEW_ZNODE);
         n.setServerStatus(s);
         fct(n);
-        System.out.println(n.getServerStatus().getTransition().name());
-        System.out.println(n.getServerStatus().getStatus().name());
-
+        assertEquals(ZNodeMessageStatus.NEW_ZNODE_RECEIVE_DATA,n.getServerStatus().getTransition().name());
+        assertEquals(ServerStatusType.MOVE_DATA_SENDER,n.getServerStatus().getStatus().name());
     }
+
 }
--- main_code/src/testing/MetadataTest.java
+++ main_code/src/testing/MetadataTest.java
@@ -1,4 +1,64 @@
 package testing;
 
-public class MetadataTest {
+import common.metadata.Metadata;
+import junit.framework.TestCase;
+
+public class MetadataTest extends TestCase {
+
+
+    public void testMetadataWrapAround(){
+        Metadata m = new Metadata();
+
+        m.addServer("a");
+        // 1695523700196324017305827155985872649
+        m.addServer("d");
+        // 195289424170611159128911017612795795343
+        m.addServer("b");
+        // 173422077530204247440288476180261147053
+        m.addServer("e");
+        // 299611584147932843547128611849858313266
+
+        // 333324391474467668306539665063131100439
+        String serverName = m.findServer("testkey2");
+
+        assertEquals("a",serverName);
+    }
+
+    public void testMetadataFindServer(){
+        Metadata m = new Metadata();
+
+        m.addServer("a");
+        // 1695523700196324017305827155985872649
+        m.addServer("d");
+        // 195289424170611159128911017612795795343
+        m.addServer("b");
+        // 173422077530204247440288476180261147053
+        m.addServer("e");
+        // 299611584147932843547128611849858313266
+
+        // 45335050332576958438065669921095352181
+        String serverName = m.findServer("testkey");
+
+        assertEquals("d",serverName);
+    }
+
+    public void testMetadataRemove(){
+        Metadata m = new Metadata();
+
+        m.addServer("a");
+        // 1695523700196324017305827155985872649
+        m.addServer("d");
+        // 195289424170611159128911017612795795343
+        m.addServer("b");
+        // 173422077530204247440288476180261147053
+        m.addServer("e");
+        // 299611584147932843547128611849858313266
+
+        m.removeServer("d");
+
+        // 45335050332576958438065669921095352181
+        String serverName = m.findServer("testkey");
+
+        assertEquals("b",serverName);
+    }
 }
